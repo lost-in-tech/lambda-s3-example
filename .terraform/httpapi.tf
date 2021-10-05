9e6fa2d8931402ffbbbbc0bd8e4feee4c8f36c18
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-10-05 23:10:33 +1100

    wip.

--- .terraform/httpapi.tf
+++ .terraform/httpapi.tf
@@ -1,7 +1,15 @@
 locals {
   api_gateway_name = "test-http2-api"
+  env              = "dev"
 }
+
 resource "aws_apigatewayv2_api" "main" {
   name          = local.api_gateway_name
   protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "main" {
+  api_id      = aws_apigatewayv2_api.main.id
+  name        = local.env
+  auto_deploy = true
 }
