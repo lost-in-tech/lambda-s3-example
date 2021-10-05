6a39935102874362469bc259da950dbe5a8bffbd
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-10-05 23:17:37 +1100

    wip.

--- .terraform/httpapi.tf
+++ .terraform/httpapi.tf
@@ -15,9 +15,9 @@ resource "aws_apigatewayv2_stage" "main" {
 }
 
 resource "aws_apigatewayv2_integration" "main" {
-  api_id           = aws_apigatewayv2_api.main.id
-  integration_type = "HTTP_PROXY"
-
+  api_id             = aws_apigatewayv2_api.main.id
+  integration_type   = "HTTP_PROXY"
+  integration_uri    = aws_lambda_function.app.invoke_arn
   integration_method = "POST"
 }
 
