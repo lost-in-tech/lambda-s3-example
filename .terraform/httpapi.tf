aa6ba4353a76c9a57838e0859dfa8f1f061959c1
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-10-06 21:46:28 +1100

    wip...

--- .terraform/httpapi.tf
+++ .terraform/httpapi.tf
@@ -79,3 +79,18 @@ resource "aws_apigatewayv2_route" "main" {
 
   target = "integrations/${aws_apigatewayv2_integration.main.id}"
 }
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.main.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
